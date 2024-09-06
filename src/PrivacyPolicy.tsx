450e4a9d6e20e7efe0938b5243abba2bd5612e14
Jackson Farnsworth <jafarns95@gmail.com>
2024-09-06 13:07:09 -0500

    Add privacy policy

---
+++ src/PrivacyPolicy.tsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+      <h1>RecipeBook Privacy Policy</h1>
+      <p>RecipeBook does not track or report data entered by the user, except by what the user's device/OS does by default. 
+        Any data backed up to Google can be deleted via the settings icon at the top right, then "Go To Drive Settings". 
+        Simply click "Delete All Data" and confirm to remove backups. Any data backed up manually by the user will initially 
+        be found in the device's "Downloads" folder, and are marked by the extension ".rp".</p>
+    </div>
+  );
+}
+
+export default About;
